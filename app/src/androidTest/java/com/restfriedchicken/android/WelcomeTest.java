3cdf8113069d87865119413a96c327d10fb8400b
hippoom <hippoom@gmail.com>
2015-03-16 12:18:20 +0800

    Add acceptance test against welcome page.

--- app/src/androidTest/java/com/restfriedchicken/android/WelcomeTest.java
+++ app/src/androidTest/java/com/restfriedchicken/android/WelcomeTest.java
@@ -1,5 +1,31 @@
 package com.restfriedchicken.android;
 
-public class EditorTest {
+import android.test.ActivityInstrumentationTestCase2;
 
+import com.robotium.solo.Solo;
+
+
+public class WelcomeTest extends
+        ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+
+    public WelcomeTest() {
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testWelcomeIsShown() throws Exception {
+
+        assertTrue(solo.searchText("Welcome!"));
+
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
 }
