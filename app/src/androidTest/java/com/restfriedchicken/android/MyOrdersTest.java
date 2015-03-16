6f16290c5cd8fc84f5c60c47fd4dccb9a3e89f41
hippoom <hippoom@gmail.com>
2015-03-16 15:02:31 +0800

    Display my orders with dummy data. #1

---
+++ app/src/androidTest/java/com/restfriedchicken/android/MyOrdersTest.java
@@ -0,0 +1,41 @@
+package com.restfriedchicken.android;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+
+public class MyOrdersTest extends
+        ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+
+    private Solo myOrders;
+
+    public MyOrdersTest() {
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testMyOrdersAreShown() throws Exception {
+
+        solo.clickOnButton("My Orders");
+
+        assertMyOrdersAreShown();
+    }
+
+    private void assertMyOrdersAreShown() {
+        assertTrue(solo.searchText("tracking_id_1"));
+        assertTrue(solo.searchText("tracking_id_2"));
+        assertTrue(solo.searchText("tracking_id_3"));
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+}
--- app/src/main/java/com/restfriedchicken/android/orders/DisplayMyOrdersActivity.java
+++ app/src/main/java/com/restfriedchicken/android/orders/DisplayMyOrdersActivity.java
@@ -1,21 +1,39 @@
-package com.restfriedchicken.android;
+package com.restfriedchicken.android.orders;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import com.restfriedchicken.android.orders.DisplayMyOrdersActivity;
+import com.restfriedchicken.android.R;
 
-
-public class MainActivity extends Activity {
+public class DisplayMyOrdersActivity extends Activity {
+    private ListView myOrdersView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_display_my_orders);
+        myOrdersView = (ListView) findViewById(android.R.id.list);
+        myOrdersView.setEmptyView(findViewById(android.R.id.empty));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, loadMyOrders());
+
+        myOrdersView.setAdapter(adapter);
+    }
+
+    private String[] loadMyOrders() {
+        return new String[] {"tracking_id_1", "tracking_id_2", "tracking_id_3"};
     }
 
 
@@ -32,18 +50,10 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    /** Called when the user clicks the My Orders button */
-    public void redirectToMyOrders(View view) {
-        Intent intent = new Intent(this, DisplayMyOrdersActivity.class);
-        startActivity(intent);
-    }
 }
