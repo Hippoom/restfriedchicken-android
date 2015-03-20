052bef4a34890cff3c7cce1c406b353178542ed0
hippoom <hippoom@gmail.com>
2015-03-20 18:02:37 +0800

    En-pass the existing tests. #3

--- app/src/androidTest/java/com/restfriedchicken/android/at/MyOrdersTest.java
+++ app/src/androidTest/java/com/restfriedchicken/android/at/MyOrdersTest.java
@@ -1,6 +1,5 @@
 package com.restfriedchicken.android.at;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.restfriedchicken.android.MainActivity;
@@ -23,19 +22,32 @@ public class MyOrdersTest extends
 
     public void testMyOrdersAreShown() throws Exception {
 
-        redirectToMyOrders();
+        when_I_navigate_to_my_orders();
 
-        assertMyOrdersAreShown();
+        then_it_should_display_my_orders_which_status_is_WAIT_PAYMENT();
+
+        when_I_swipe_to_view_the_orders_that_are_being_SERVING();
+
+        then_it_should_display_my_orders_which_status_is_SERVING();
     }
 
-    private void redirectToMyOrders() {
+    private void when_I_swipe_to_view_the_orders_that_are_being_SERVING() {
+        orderFixture.getSolo().scrollToSide(Solo.RIGHT);
+        orderFixture.getSolo().sleep(2);
+    }
+
+    private void then_it_should_display_my_orders_which_status_is_SERVING() {
+
+        assertTrue(getSolo().searchText("tracking_id_2", true));
+        assertTrue(getSolo().searchText("tracking_id_3", true));
+    }
+
+    private void when_I_navigate_to_my_orders() {
         orderFixture.redirectToMyOrders();
     }
 
-    private void assertMyOrdersAreShown() {
-        assertTrue(getSolo().searchText("tracking_id_1"));
-        assertTrue(getSolo().searchText("tracking_id_2"));
-        assertTrue(getSolo().searchText("tracking_id_3"));
+    private void then_it_should_display_my_orders_which_status_is_WAIT_PAYMENT() {
+        assertTrue(getSolo().searchText("tracking_id_1", true));
     }
 
     @Override
